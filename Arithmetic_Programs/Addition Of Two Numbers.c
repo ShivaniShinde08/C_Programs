75066c0fa337cf86dc62ff86212d3c36218571a7
ShivaniShinde08 <114905268+ShivaniShinde08@users.noreply.github.com>
2022-10-26 15:48:05 +0530

    Add files via upload

---
+++ Arithmetic_Programs/Addition Of Two Numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+  int No1 = 0,No2 = 0,Sum = 0;
+  
+  printf("\n\t Enter Two Numbers For Addition => ",No1, No2);
+  scanf("%d%d",&No1,&No2);
+  
+  Sum = No1 + No2;
+  
+  printf("\n\t Addition Of %d & %d  Is => %d",No1,No2,Sum);
+  
+  getch();
+  return 0;
+}
