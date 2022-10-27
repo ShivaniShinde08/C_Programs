af33cbbf98cba032ca6c68b87c2e09ff232dd86b
ShivaniShinde08 <114905268+ShivaniShinde08@users.noreply.github.com>
2022-10-27 10:32:11 +0530

    Add files via upload

---
+++ Arithmetic_Programs/02_Subtraction_Of_Two_ Numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+  int No1 = 0,No2 = 0,Sub = 0;
+  
+  up:
+    printf("\n\t Enter Two Numbers For Subtraction => ");
+    scanf("%d%d",&No1,&No2);
+  
+    if(No1 <= No2 || No1 == 0 || No2 == 0)
+    {
+       goto up;
+    }
+  
+    Sub = No1 - No2;
+  
+    printf("\n\t Subtraction Of %d & %d => %d",No1,No2,Sub);
+  
+  getch();
+  return 0;
+}
