d6ec1818b9008908fa5ecdc172088555901b0dcc
ShivaniShinde08 <114905268+ShivaniShinde08@users.noreply.github.com>
2022-10-29 22:29:51 +0530

    Add files via upload

---
+++ Arithmetic_Programs/04_Division_Of_Two_Number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+  int No1 = 0,No2 = 0,Ans = 0;
+  
+  printf("\n\t Enter Two Numbers => ");
+  scanf("%d%d",&No1,&No2);
+  
+  Ans = No1 / No2;
+  
+  printf("\n\t Division Of %d & %d Is => %d",No1,No2,Ans);
+  
+  getch();
+  return 0;
+}
