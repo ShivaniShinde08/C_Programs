cb78dde26905d950b4c8d2f8b0d9c9a3116187fc
ShivaniShinde08 <114905268+ShivaniShinde08@users.noreply.github.com>
2022-10-30 17:50:59 +0530

    Add files via upload

---
+++ Condition_Control_statement/If/Addition_Of_Two_Program_Using_If.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+  int No1 = 0,No2 = 0,Ans = 0;
+  
+  up:
+    
+    printf("\n\t Enter Two Number => ");
+    scanf("%d%d",&No1,&No2);
+  
+    if(No1 == 0 || No2 == 0)
+    {
+      goto up;
+    }
+    
+    Ans = No1 + No2;
+    
+    printf("Addition Of %d & %d => %d",No1,No2,Ans);
+    
+  getch();
+  return 0;
+}
