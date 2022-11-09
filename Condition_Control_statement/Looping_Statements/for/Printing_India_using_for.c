4371c317ec273c91eb6b0eddfd34d824405d84f8
ShivaniShinde08 <114905268+ShivaniShinde08@users.noreply.github.com>
2022-11-09 21:41:04 +0530

    Add files via upload

---
+++ Condition_Control_statement/Looping_Statements/for/Printing_India_using_for.c
@@ -0,0 +1,20 @@
+/// Print n Number Of India.... 
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+  int No = 0,i = 0;
+  
+  printf("\n\t Enter A Number => ");
+  scanf("%d",&No);
+  
+  for(i = 1;i <= No;i++)
+  {
+      printf("\n\t India. ");
+  }
+  
+  getch();
+  return 0;
+}
