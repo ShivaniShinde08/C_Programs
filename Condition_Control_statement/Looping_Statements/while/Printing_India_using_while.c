20906d820bc8e7a3bd8de3517dc651a0e2170d39
ShivaniShinde08 <114905268+ShivaniShinde08@users.noreply.github.com>
2022-11-09 21:43:42 +0530

    Add files via upload

---
+++ Condition_Control_statement/Looping_Statements/while/Printing_India_using_while.c
@@ -0,0 +1,21 @@
+/// Print n Number Of India.... 
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+  int No = 0,i = 0;
+  
+  printf("\n\t Enter A Number => ");
+  scanf("%d",&No);
+  
+  while(i < No)
+  {
+      printf("\n\t India. ");
+      i++;
+  }
+  
+  getch();
+  return 0;
+}
