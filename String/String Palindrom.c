97e0ce0b5f9843bb63d729b8de92ffb19e04cbbc
ShivaniShinde08 <114905268+ShivaniShinde08@users.noreply.github.com>
2022-11-14 22:55:29 +0530

    Add files via upload

---
+++ String/String Palindrom.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0,j = 0;
+    char Csrc[20] = {},Cdst[20] = {};
+
+    printf("\n\t Enter A String =>  ");
+    scanf("%s",&Csrc);
+
+    for(j = 0;Csrc[j] != '\0';j++);
+
+    j--;
+
+    for(i = 0;Csrc[i] != '\0';i++,j--)
+    {
+        Cdst[i] = Csrc[j];
+    }
+
+    printf("\n\t Reverse String Is %s",Cdst);
+
+    getch();
+    return 0;
+}
+
