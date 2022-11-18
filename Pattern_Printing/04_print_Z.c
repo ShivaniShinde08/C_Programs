e2578db5891a51fa3e6de2f0c01eaa8723c96502
ShivaniShinde08 <114905268+ShivaniShinde08@users.noreply.github.com>
2022-11-18 20:27:12 +0530

    Add files via upload

---
+++ Pattern_Printing/04_print_Z.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+  int size = 5,r = 0,c = 0;
+  
+  for(r = 1;r <= size;r++)
+  {
+      for(c = 1;c <= size;c++)
+      {
+          if(r + c == (size+1) || r == 1 || r == size)
+          {
+              printf(" *");
+          }   
+          else
+          {  
+              printf("  ");   
+          }  
+      }
+      printf("\n");
+  }
+  getch();
+  return 0;
+}
