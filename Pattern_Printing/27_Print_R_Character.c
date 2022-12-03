d7c699dc73ddfcb750581fa1986aa5503954bb03
ShivaniShinde08 <114905268+ShivaniShinde08@users.noreply.github.com>
2022-12-03 19:26:55 +0530

    Add files via upload

---
+++ Pattern_Printing/27_Print_R_Character.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+         int r = 0, c = 0, Size = 0;
+
+         printf("\n Enter A Size => ");
+         scanf("%d",&Size);
+
+         printf("\n\n pattern is =>\n\n");
+
+         for(r = 1; r <= Size; r++)
+         {
+                  for(c = 1; c <= Size; c++)
+                  {
+                           if(c == 1 || r == 1 && c < Size || c == Size && r < (Size+1)/2 && r >1 || r == (Size+1)/2 && c < Size || r-c == ((Size+1)/2-1))
+                           {
+                                 printf(" *");
+                           }
+                           else
+                           {
+                                 printf("  ");
+                           }
+                  }
+                  printf("\n");
+         }
+
+         printf("\n\n Thank You !!!\n");
+
+         getch();
+         return 0;
+}
