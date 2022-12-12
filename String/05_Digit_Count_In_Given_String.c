97c6d12e3cd2e2845bc1fe78d5611047a8e87094
ShivaniShinde08 <114905268+ShivaniShinde08@users.noreply.github.com>
2022-12-12 21:54:22 +0530

    Add files via upload

---
+++ String/05_Digit_Count_In_Given_String.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>>
+
+int main()
+{
+        char cSrc[50] = {'\0'};
+        int Cnt = 0, i = 0;
+
+        printf("\n Enter a String = ");
+        gets(cSrc);
+
+        while(cSrc[i] != '\0')
+        {
+                if(cSrc[i] >= '0' && cSrc[i] <= '9')
+                {
+                    Cnt++;
+                }
+                i++;
+        }
+
+        printf("\n The Digits Count in Given String = %d",Cnt);
+
+        printf("\n\n Thank You !!!\n");
+
+        getch();
+        return 0;
+}
